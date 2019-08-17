6e341d10b9fa154b5cf2a20806bb378ee5cda6c3
shubham3597 <vshubham5636@gmail.com>
2019-08-17 14:53:01 +0530

    changes

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: AppComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
